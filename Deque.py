6d1075a54d04af0944446ffc5f418af49a87f3c1
Rus <rust66637@gmail.com>
2020-07-27 01:48:32 +0300

    Update Deque.py

--- Deque.py
+++ Deque.py
@@ -1,20 +1,24 @@
 class Deque:
     def __init__(self):
-        # инициализация внутреннего хранилища
+        self.deque = [] # инициализация внутреннего хранилища
 
     def addFront(self, item):
-        # добавление в голову
+        self.deque.insert(0, item) # добавление в голову
 
     def addTail(self, item):
-        # добавление в хвост
+        self.deque.append(item) # добавление в хвост
 
     def removeFront(self):
-        # удаление из головы
-        return None # если очередь пуста
+        if self.size:
+            self.deque.pop(0) # удаление из головы
+        else:
+            return None # если очередь пуста
 
     def removeTail(self):
-        # удаление из хвоста
-        return None # если очередь пуста
+        if self.size:
+            self.deque.pop() # удаление из хвоста
+        else:
+            return None # если очередь пуста
 
     def size(self):
-        return 0 # размер очереди
+        return len(self.deque) # размер очереди
